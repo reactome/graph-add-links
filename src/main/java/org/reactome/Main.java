8f0d6d5ef9c1af373004c0ef0fb04e4b9e3fe967
Joel Weiser <jweiser13@protonmail.com>
2024-01-03 18:36:37 -0500

    calls resources retriever, fileprocessor, and referencecreator by reflection

--- src/main/java/org/reactome/Main.java
+++ src/main/java/org/reactome/Main.java
@@ -3,39 +3,14 @@ package org.reactome;
 import org.json.JSONObject;
 import org.reactome.referencecreators.ReferenceCreator;
 import org.reactome.resource.FileProcessor;
-import org.reactome.resource.FileRetriever;
 import org.reactome.resource.Retriever;
-import org.reactome.resource.biogpsgene.BioGPSGeneFileProcessor;
-import org.reactome.resource.biogpsgene.BioGPSGeneFileRetriever;
-import org.reactome.resource.biogpsgene.BioGPSGeneReferenceCreator;
-import org.reactome.resource.complexportal.ComplexPortalFileProcessor;
-import org.reactome.resource.complexportal.human.ComplexPortalHumanFileRetriever;
-import org.reactome.resource.complexportal.human.ComplexPortalHumanReferenceCreator;
-import org.reactome.resource.complexportal.sars.ComplexPortalSARSFileRetriever;
-import org.reactome.resource.ctdgene.CTDGeneFileProcessor;
-import org.reactome.resource.ctdgene.CTDGeneFileRetriever;
-import org.reactome.resource.ctdgene.CTDGeneReferenceCreator;
-import org.reactome.resource.flybase.FlyBaseFileProcessor;
-import org.reactome.resource.flybase.FlyBaseFileRetriever;
-import org.reactome.resource.flybase.FlyBaseReferenceCreator;
-import org.reactome.resource.glygen.GlyGenFileProcessor;
-import org.reactome.resource.glygen.GlyGenFileRetriever;
-import org.reactome.resource.omim.OMIMFileRetriever;
-import org.reactome.resource.opentargets.OpenTargetsFileProcessor;
-import org.reactome.resource.opentargets.OpenTargetsFileRetriever;
-import org.reactome.resource.orphanet.OrphanetFileProcessor;
-import org.reactome.resource.orphanet.OrphanetFileRetriever;
-import org.reactome.resource.pdb.PDBFileRetriever;
-import org.reactome.resource.pro.PROFileProcessor;
-import org.reactome.resource.pro.PROFileRetriever;
-import org.reactome.resource.pro.PROReferenceCreator;
-import org.reactome.resource.vgnc.VGNCFileProcessor;
-import org.reactome.resource.vgnc.VGNCFileRetriever;
-import org.reactome.resource.vgnc.VGNCReferenceCreator;
-import org.reactome.resource.zfin.ZFINFileRetriever;
+import org.reactome.resource.pharmacodb.PharmacoDBFileProcessor;
 import org.reactome.utils.ResourceJSONParser;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
 import java.util.Map;
 
 /**
@@ -43,91 +18,48 @@ import java.util.Map;
  *         Created 4/8/2022
  */
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws
+        IOException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException,
+        InvocationTargetException, InstantiationException, URISyntaxException {
+
         Map<String, JSONObject> resourceNameToResourceObject = ResourceJSONParser.getResourceJSONObjects();
 
 
-
-        Retriever retriever = new CTDGeneFileRetriever();
-        //retriever.downloadFile();
-        FileProcessor fileProcessor = new CTDGeneFileProcessor(retriever.getLocalFilePath());
-        System.out.println(fileProcessor.getSourceToResourceIdentifiers());
-        ReferenceCreator referenceCreator = new CTDGeneReferenceCreator(fileProcessor.getSourceToResourceIdentifiers());
-        referenceCreator.insertIdentifiers();
-//        Retriever retriever = new ComplexPortalHumanFileRetriever();
-//        retriever.downloadFile();
-//        FileProcessor fp = new ComplexPortalFileProcessor(retriever.getLocalFilePath());
-//        System.out.println(fp.getSourceToResourceIdentifiers());
-//        ReferenceCreator rc = new ComplexPortalHumanReferenceCreator(fp.getSourceToResourceIdentifiers());
-//        rc.insertIdentifiers();
-//        Retriever retriever = new GlyGenFileRetriever();
-//        retriever.downloadFile();
-//        FileProcessor fp = new GlyGenFileProcessor(retriever.getLocalFilePath());
-//        System.out.println(fp.getSourceToResourceIdentifiers());
-//
-//        System.exit(0);
         for (String resourceName : resourceNameToResourceObject.keySet()) {
-
-
-        }
-//            FileRetriever fileRetriever = new VGNCFileRetriever();
-//            //fileRetriever.downloadFile();
-//            FileProcessor fileProcessor = new VGNCFileProcessor(fileRetriever.getLocalFilePath());
-//            System.out.println(fileProcessor.getSourceToResourceIdentifiers());
-//            ReferenceCreator referenceCreator = new VGNCReferenceCreator(fileProcessor.getSourceToResourceIdentifiers());
-//            referenceCreator.insertIdentifiers();
-
-
-//            String resourceName = "PRO";
-//            JSONObject resourceJSONObject = resourceNameToResourceObject.get(resourceName);
-
-//            FileRetriever flyBaseFileRetriever = new FlyBaseFileRetriever();
-//            flyBaseFileRetriever.downloadFile();
-            //System.out.println("Creating file retriever...");
-            //FileRetriever proFileRetriever = new PROFileRetriever();
-            //System.out.println("Downloading file...");
-            //proFileRetriever.downloadFile();
-
-//            FileProcessor flyBaseFileProcessor = new FlyBaseFileProcessor(flyBaseFileRetriever.getLocalFilePath());
-//            System.out.println("Creating file processor...");
-            //FileProcessor proFileProcessor = new PROFileProcessor(proFileRetriever.getLocalFilePath());
-
-
-//            System.out.println("Creating database identifier reference creator...");
-//            ReferenceCreator flyBaseReferenceCreator = new FlyBaseReferenceCreator(flyBaseFileProcessor.getSourceToResourceIdentifiers());
-
-            //ReferenceCreator proReferenceCreator =
-            //    new PROReferenceCreator(proFileProcessor.getSourceToResourceIdentifiers());
-            //System.out.println(databaseIdentifierReferenceCreator.getReferenceDatabase().toString());
-
-//            System.out.println("Inserting database identifiers...");
-            //proReferenceCreator.insertIdentifiers();
-//        flyBaseReferenceCreator.insertIdentifiers();
-        //}
-        System.exit(0);
-//
-//        Collection<ReferenceGeneProduct> referenceGeneProducts =
-//            ReferenceGeneProduct.fetchAllReferenceGeneProducts().values();
-
-//        FileRetriever fileRetriever = new FileRetriever();
-        //System.out.println(fileRetriever.getFlyBaseFileRemoteURL().toString());
-//        fileRetriever.downloadFile();
-
-//        //System.out.println(fileRetriever.getLocalFilePath());
-//        FlyBaseFileProcessor fileProcessor = new FlyBaseFileProcessor(fileRetriever.getLocalFilePath());
-//        DatabaseIdentifierReferenceCreator databaseIdentifierReferenceCreator =
-//            new DatabaseIdentifierReferenceCreator("FlyBase", fileProcessor.getUniProtToFlyBase());
-
-//        for (ReferenceGeneProduct referenceGeneProduct : referenceGeneProducts) {
-//            List<DatabaseIdentifier> databaseIdentifiers =
-//                databaseIdentifierReferenceCreator.createDatabaseIdentifiersForReferenceGeneProduct(referenceGeneProduct);
-//
-//            for (DatabaseIdentifier databaseIdentifier : databaseIdentifiers) {
-//                referenceGeneProduct.connectTo(databaseIdentifier, new GraphNode.Relationship("crossReference"));
+//            if (!resourceName.equals("COSMIC")) {
+//                continue;
 //            }
-//        }
 
-        //fileProcessor.getUniProtToFlyBase()
-        //System.out.println(fileProcessor.getUniProtToFlyBase());
+            String resourcePackage = "org.reactome.resource." + resourceName.toLowerCase();
+            String resourceFileRetriever = resourcePackage + "." + resourceName + "FileRetriever";
+            System.out.println(resourceFileRetriever);
+
+            Retriever retriever = (Retriever) Class.forName(resourceFileRetriever).getDeclaredConstructor().newInstance();
+            retriever.downloadFiles();
+
+            String resourceFileProcessor = resourcePackage + "." + resourceName + "FileProcessor";
+            System.out.println(resourceFileProcessor);
+            System.out.println(retriever.getLocalFilePaths().get(0));
+            FileProcessor fileProcessor = getFileProcessor(resourceFileProcessor, retriever);
+            System.out.println(fileProcessor.getSourceToResourceIdentifiers());
+
+            String resourceReferenceCreator = resourcePackage + "." + resourceName + "ReferenceCreator";
+            ReferenceCreator referenceCreator = (ReferenceCreator) Class.forName(resourceReferenceCreator).getDeclaredConstructor(Map.class).newInstance(fileProcessor.getSourceToResourceIdentifiers());
+
+            referenceCreator.insertIdentifiers();
+        }
+    }
+
+    private static FileProcessor getFileProcessor(String resourceFileProcessor, Retriever retriever)
+        throws ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+
+        if (resourceFileProcessor.contains("PharmacoDB")) {
+            Path[] filePaths = retriever.getLocalFilePaths().toArray(new Path[0]);
+
+            return new PharmacoDBFileProcessor(filePaths);
+        }
+
+        return (FileProcessor) Class.forName(resourceFileProcessor).getDeclaredConstructor(Path.class)
+            .newInstance(retriever.getLocalFilePaths().get(0));
     }
 }
