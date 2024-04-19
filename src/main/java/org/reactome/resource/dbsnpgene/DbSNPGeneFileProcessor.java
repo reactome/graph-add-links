579d6b8b3ca34d74980c6ac4e5243b2ca6b1e7f7
Joel Weiser <jweiser13@protonmail.com>
2024-04-19 16:36:17 -0400

    filter dbSNPGene for human ReferenceGeneProducts only

--- src/main/java/org/reactome/resource/dbsnpgene/DbSNPGeneFileProcessor.java
+++ src/main/java/org/reactome/resource/dbsnpgene/DbSNPGeneFileProcessor.java
@@ -4,14 +4,36 @@ import org.reactome.resource.UniProtFileProcessor;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.reactome.graphnodes.ReferenceGeneProduct.fetchHumanReferenceGeneProducts;
 
 /**
  * @author Joel Weiser (joel.weiser@oicr.on.ca)
  *         Created 11/22/2023
  */
 public class DbSNPGeneFileProcessor extends UniProtFileProcessor {
+    private Set<String> humanReferenceGeneProducts;
 
     public DbSNPGeneFileProcessor(Path filePath) throws IOException {
         super(filePath);
+    }
+
+    @Override
+    public Map<String, Set<String>> getSourceToResourceIdentifiers() throws IOException {
+        Map<String, Set<String>> uniProtToResourceIdentifiers = super.getSourceToResourceIdentifiers();
+        uniProtToResourceIdentifiers.entrySet().removeIf(entry -> !isHuman(entry));
+        return uniProtToResourceIdentifiers;
+    }
+
+    private boolean isHuman(Map.Entry<String, Set<String>> uniProtToResourceIdentifierEntry) {
+        String uniProtAccession = uniProtToResourceIdentifierEntry.getKey();
+        if (this.humanReferenceGeneProducts == null || this.humanReferenceGeneProducts.isEmpty()) {
+            this.humanReferenceGeneProducts = fetchHumanReferenceGeneProducts().keySet();
+        }
+
+        return this.humanReferenceGeneProducts.contains(uniProtAccession);
     }
 }
