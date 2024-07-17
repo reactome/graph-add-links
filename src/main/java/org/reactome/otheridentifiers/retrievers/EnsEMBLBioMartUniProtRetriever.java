ef560d5bfe73a3e2cd2a885870650b05837e404f
Joel Weiser <jweiser13@protonmail.com>
2024-07-16 22:59:56 -0400

    updates EnsEMBLBioMartUniProtRetriever to use downloadable in constructor

--- src/main/java/org/reactome/otheridentifiers/retrievers/EnsEMBLBioMartUniProtRetriever.java
+++ src/main/java/org/reactome/otheridentifiers/retrievers/EnsEMBLBioMartUniProtRetriever.java
@@ -1,11 +1,9 @@
 package org.reactome.otheridentifiers.retrievers;
 
 import org.reactome.DownloadInfo;
-import org.reactome.resource.Retriever;
+import org.reactome.resource.SingleRetriever;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.reactome.otheridentifiers.EnsEMBLBioMartUtils.*;
@@ -14,28 +12,17 @@ import static org.reactome.otheridentifiers.EnsEMBLBioMartUtils.*;
  * @author Joel Weiser (joel.weiser@oicr.on.ca)
  * Created 7/8/2024
  */
-public class EnsEMBLBioMartUniProtRetriever implements Retriever {
-    private DownloadInfo downloadInfo;
+public class EnsEMBLBioMartUniProtRetriever extends SingleRetriever {
 
-    public EnsEMBLBioMartUniProtRetriever(String resourceName) {
-        this.downloadInfo = new DownloadInfo(resourceName);
+    public EnsEMBLBioMartUniProtRetriever(DownloadInfo.Downloadable downloadable) {
+        super(downloadable);
     }
 
     @Override
-    public void downloadFile(DownloadInfo.Downloadable downloadable) throws IOException {
-        for (String uniProtSearchTerm : downloadable.getSearchTerms()) {
-            List<String> bioMartResponse = queryBioMart(bioMartSpeciesName, uniProtSearchTerm);
-            saveFile(getFilePath(downloadable, bioMartSpeciesName), bioMartResponse);
+    public void downloadFile() throws IOException {
+        for (String uniProtSearchTerm : getDownloadable().getSearchTerms()) {
+            List<String> bioMartResponse = queryBioMart(getDownloadable().getSpecies(), uniProtSearchTerm);
+            saveFile(getDownloadable().getLocalFilePath(), bioMartResponse);
         }
     }
-
-    @Override
-    public DownloadInfo getDownloadInfo() {
-        return this.downloadInfo;
-    }
-
-    private Path getFilePath(DownloadInfo.Downloadable downloadable, String bioMartSpeciesName) {
-        return getOutputDirectory().resolve(downloadable.getLocalFileName().replace("\\w+", bioMartSpeciesName));
-    }
-
 }
